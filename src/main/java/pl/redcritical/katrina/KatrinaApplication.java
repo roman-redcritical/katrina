9277f7897c6650142c591b0ef209ca0ea330992c
Roman Koval <RO31280T@wipro.com>
2018-09-04 21:40:22 +0200

    init commit

---
+++ src/main/java/pl/redcritical/katrina/KatrinaApplication.java
@@ -0,0 +1,12 @@
+package pl.redcritical.katrina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KatrinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KatrinaApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/redcritical/katrina/KatrinaApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.redcritical.katrina;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KatrinaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
